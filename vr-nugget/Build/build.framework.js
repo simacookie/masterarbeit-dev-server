0156d2fa4131eef330ae690298963c9a98ce5bba
Simon <alers@evospark.de>
2025-10-24 15:23:05 +0200

    test

---
+++ vr-nugget/Build/build.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a70e3b56a1ff17959f61641c3f0ea8e7fea1383de640adf558bd59f1f5ce8fc4
+size 440293
---
+++ vr-nugget/Build/build.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1ec4b6213aed23eb60c0cace9a151f08d3fc8363441eeb46b530a250711f697
+size 20168
