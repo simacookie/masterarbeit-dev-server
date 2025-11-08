8ddef0fa6262e6f02144a733b431e667fc5bc971
Simon <alers@evospark.de>
2025-11-08 16:19:33 +0100

    updated vr-nugget build

--- vr-nugget/Build/vr-nugget.framework.js
+++ vr-nugget/Build/vr-nugget.framework.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a5ca1dae82698f4339fb58c4d2599a8a8eec69aaecedf2e71975154a5231232
+oid sha256:86dc1b20b6642bcadd5fe06cc7eb455e541182391932609149408a2125be9e93
 size 446563
